206bce5fca9aa69a57b39a0a26d0411b7fa5b6ad
s1h1ikha <shikha11051996@gmail.com>
2017-06-29 22:03:57 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+var quotes = ['The trickster\'s function is to break taboos, create mischief, stir things up. In the end, the trickster gives people what they really want, some sort of freedom. - Tom Robbins',
+  'Disbelief in magic can force a poor soul into believing in government and business. - Tom Robbins',
+  'The trouble with the fast lane is that all the movement is horizontal. And I like to go vertical sometimes. - Tom Robbins',
+  'Our world isn\'t made of earth, air and water or even molecules and atoms; our world is made of language. - Tom Robbins',
+  'I\'m not infatuated with frivolousness. We\'re just good friends. - Tom Robbins',
+  'In fiction, when you paint yourself into a corner, you can write a pair of suction cups onto the bottoms of your shoes and walk up the wall and out the skylight and see the sun breaking through the clouds. In nonfiction, you don\'t have that luxury. - Tom Robbins',]
+function newQuote() {
+var randomNumber = Math.floor(Math.random() * (quotes.length));
+document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
+function tweet(message) {
+  window.open('https://twitter.com/intent/tweet?hashtags= freecodecamp&text='   + encodeURIComponent(message));
+}
+var msg;
+function tweetHandler() {
+  tweet($(msg).text()); 
+  //tweet(msg);
+}
+$(document).ready(function() {
+  $("#getMessage").on("click", function(){
+    var randomquote = Math.floor(Math.random()*(quote.length-1));
+    msg = quotes[randomquote];
+    $(".message").html(msg);
+  });
+
+  $('#tweetMessage').on('click', tweetHandler);
+
+});
